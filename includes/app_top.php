41f0011400587b8c9fcb2032857b3b03b62b02b6
Jeremie J. Jarosh <jeremie@jarosh.org>
2014-02-22 17:03:13 -0600

    Update the features list on the homepage

--- includes/app_top.php
+++ includes/app_top.php
@@ -5,6 +5,6 @@ require('includes/prescription/px_template.php');
 PX_Template::set_template_dir( dirname(dirname(__FILE__))."/templates/" );
 PX_Template::set_template("default");
 
-define('HERI_VERSION', "0.2.1a");
+define('HERI_VERSION', "0.2.2a");
 
 define('DIR_EXTENSIONS', "extensions/");
--- pages/home.php
+++ pages/home.php
@@ -7,7 +7,7 @@ ob_start();
 ?>
 
 	<h1>Heavy<em>er</em> Ink <sup><small style="color:black">alpha</small></sup></h1>
-	<p>Install the <a href="heavyerink.user.js">User Script</a> to get the most out of <b>Heavy Ink</b>.
+	<p>Install the <a href="heavyerink.user.js">User Script</a> to get the most out of <b>HeavyInk</b>.
 		<br/><sup><small style="padding-left:1em;">(Current version: <?=HERI_VERSION;?>)</small></sup></p>
 	<p><small>Tested on Chrome and Firefox (Firefox requires
 		<a href="http://www.greasespot.net/" target="_blank">Greasemonkey</a>).</p>
@@ -16,12 +16,12 @@ ob_start();
 		<h2>Features</h2>
 		<div>
 			<div class="ftr_imgs">
-				<div><h4>New Buttons</h4><img src="imgs/ftr-btns.jpg" alt="New Buttons"/></div>
+				<!--div><h4>New Buttons</h4><img src="imgs/ftr-btns.jpg" alt="New Buttons"/></div-->
 				<div><h4>Large Cover Views</h4><img src="imgs/ftr-cvrs.jpg" alt="Large Cover Views"/></div>
-				<div><h4>Front Page Quotes</h4><img src="imgs/ftr-quos.jpg" alt="Front Page Quotes"/></div>
-				<div><h4>Add to cart</h4><img src="imgs/ftr-cart.jpg" alt="Cart"/></div>
+				<!--div><h4>Front Page Quotes</h4><img src="imgs/ftr-quos.jpg" alt="Front Page Quotes"/></div-->
+				<!--div><h4>Add to cart</h4><img src="imgs/ftr-cart.jpg" alt="Cart"/></div-->
 			</div>
-			<div class="clear-last"><h4>and more&hellip;</h4></div>
+			<div class="clear-last"><h4>More to come&hellip;</h4></div>
 		</div>
 	</div>
 
