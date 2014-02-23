9de546f3413834e47a1cd281cfeb155a3a74e0cb
Jeremie J. Jarosh <jeremie@jarosh.org>
2014-02-23 17:54:52 -0600

    Update 'debug' page
    - display the '$_SERVER' variable
    
    - calculate the size of the userscript

--- pages/debug.php
+++ pages/debug.php
@@ -3,20 +3,68 @@ require('includes/app_top.php');
 
 header("HTTP/1.0 404 Not Found");
 
+function print_arr( $array, $caption = null ) {
+	$html = "<table class=\"print_arr\">";
+	if ( $caption ) $html .= "<caption>{$caption}</caption>";
+	foreach ( $array as $key=>$value ) {
+		$html .= "<tr><th>{$key}</th><td>{$value}</td></tr>";
+	}
+	$html .= "</table>";
+
+	return $html;
+}
+
 $heri_selected_tab = "[ debug ]";
 
 ob_start();
 ?>
 
 	<script type="text/javascript">
+		/** http://stackoverflow.com/a/5015930 **/
+		function getUTF8Length( string ) {
+			var utf8length = 0;
+			for (var n = 0; n < string.length; n++) {
+				var c = string.charCodeAt(n);
+				if (c < 128) {
+					utf8length++;
+				} else if((c > 127) && (c < 2048)) {
+					utf8length = utf8length+2;
+				} else {
+					utf8length = utf8length+3;
+				}
+			}
+			return utf8length;
+		}
+
 		$(document).ready(function(){
-			$('#userscript').load("heavyerink.user.js");
+			$('#userscript').load("heavyerink.user.js", function(){
+				var filesize = getUTF8Length( $('#userscript').val() );
+				$('#userscript-data').html( "filesize: ~" + (filesize/1024).toFixed(3) + " KB" );
+			});
 		});
 	</script>
 
 	<style type="text/css">
 		#userscript {
 			font-family: monospace;
+		}
+
+		table.print_arr {
+			border-collapse: collapse
+		}
+		table.print_arr caption {
+			font-size: 1.1em;
+			font-weight: bold;
+		}
+		table.print_arr th {
+			background-color: #C2A27D;
+			text-align: left;
+		}
+		table.print_arr th,
+		table.print_arr td {
+			font-size: 0.75em;
+			border: 1px solid #947C5F;
+			padding: 0.2em 0.3em;
 		}
 	</style>
 
@@ -31,6 +79,11 @@ ob_start();
 
 	<textarea id="userscript" cols="80" rows="24">
 	</textarea>
+	<div id="userscript-data"></div>
+	<br />
+
+	<h2>Variables</h2>
+	<?=print_arr( $_SERVER, "_SERVER" );?>
 
 <?php
 $tmpl_body = ob_get_clean();
