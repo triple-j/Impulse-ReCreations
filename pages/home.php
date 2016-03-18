09d1e869ad6effe40f0aa66bb6cc29e164d2f87c
Jeremie Jarosh <jeremie@jarosh.org>
2016-03-18 02:19:07 +0000

    Update Homepage

--- pages/home.php
+++ pages/home.php
@@ -16,11 +16,26 @@ ob_start();
 
 	<div class="features">
 		<h2>Features</h2>
-		<div>
-			<ul>
-				<li>Subscription Search</li>
-			</ul>
-		</div>
+
+		<h3>New Functionality</h3>
+
+		<ul>
+			<li><em>Subscription Search</em> &mdash; Narrow down comic titles in the
+				'Reserve System' selection fields based on search terms.</li>
+			<li>Disable and highlight subscribed titles in the 'Reserve System' selection
+				fields.</li>
+		</ul>
+
+		<h3>UI Tweaks</h3>
+
+		<ul>
+			<li>Correct the text alginment of the comic description that pops up when hovering
+				over a comic issue.</li>
+			<li>Make individual rows of items (subscribed titles, orders, etc.) easier to
+				discern.</li>
+			<li>Remove excess padding between the Header/Footer and the page content.</li>
+			<li>Remove the page name the shows up on the top of some pages.</li>
+		</ul>
 	</div>
 
 <?php
